6e6ab78a8bc37181ebf94af96f5342f62f44882b
decarp0 <47693309+decarp0@users.noreply.github.com>
2025-09-07 18:51:12 +0200

    refactoring

--- src/extension.ts
+++ src/extension.ts
@@ -4,63 +4,86 @@ import * as vscode from "vscode";
 import * as fs from "fs";
 import * as path from "path";
 
+const COMMAND = "jsonfinder.findJsonKeyByPath";
+const INPUT = {
+  prompt: "Enter JSON key path",
+};
+const INCLUDED_FILES = "**/*.json";
+const EXCLUDED_FILES = "{**/node_modules/**,**/dist/**,**/build/**}";
+
 export function activate(context: vscode.ExtensionContext) {
-  let disposable = vscode.commands.registerCommand(
-    "jsonfinder.findJsonKeyByPath",
-    async () => {
-      const key = await vscode.window.showInputBox({
-        prompt: "Enter JSON key path",
-      });
-      if (!key) return;
+  let disposable = vscode.commands.registerCommand(COMMAND, async () => {
+    const key = await vscode.window.showInputBox(INPUT);
+    if (!key) return;
 
-      const files = await vscode.workspace.findFiles(
-        "**/*.json",
-        "{**/node_modules/**,**/dist/**,**/build/**}"
-      );
-      let found = false;
+    const files = await vscode.workspace.findFiles(
+      INCLUDED_FILES,
+      EXCLUDED_FILES
+    );
 
-      for (const file of files) {
-        console.debug("file: ", file);
-        try {
-          const content = fs.readFileSync(file.fsPath, "utf8");
-          const json = JSON.parse(content);
-          const value = findValue(json, key.split("."));
-
-          if (value !== undefined) {
-            const document = await vscode.workspace.openTextDocument(
-              file.fsPath
-            );
-            const editor = await vscode.window.showTextDocument(document);
-
-            // Find the line number containing the key
-            let lineNumber = findLineNumber(content, key.split("."));
-
-            if (lineNumber !== -1) {
-              const position = new vscode.Position(lineNumber, 0);
-              editor.selection = new vscode.Selection(position, position);
-              editor.revealRange(
-                new vscode.Range(position, position),
-                vscode.TextEditorRevealType.InCenter
-              );
-            }
-
-            vscode.window.showInformationMessage(
-              `Found value: ${value} in ${file.fsPath}:${lineNumber}`
-            );
-            found = true;
-            break;
-          }
-        } catch (error) {
-          console.error(`Error reading or parsing ${file.fsPath}:`, error);
-        }
-      }
-      if (!found) {
-        vscode.window.showWarningMessage(`Path '${key}' not found.`);
-      }
-    }
-  );
+    performSearch(files, key);
+  });
 
   context.subscriptions.push(disposable);
+}
+
+function performSearch(files: vscode.Uri[], key: string) {
+  let found = false;
+
+  for (const file of files) {
+    found = searchInFile(file, key);
+    if (found) break;
+  }
+
+  if (!found) {
+    vscode.window.showWarningMessage(`Path '${key}' not found.`);
+  }
+}
+
+function searchInFile(file: vscode.Uri, key: string) {
+  try {
+    const content = fs.readFileSync(file.fsPath, "utf8");
+    const json = JSON.parse(content);
+    const value = findValue(json, key.split("."));
+
+    if (value !== undefined) {
+      const lineNumber = navigateToResult(file, content, key);
+
+      vscode.window.showInformationMessage(
+        `Found value: ${value} in ${file.fsPath}:${lineNumber}`
+      );
+      return true;
+    }
+  } catch (error) {
+    console.error(`Error reading or parsing ${file.fsPath}:`, error);
+  }
+  return false;
+}
+
+async function navigateToResult(
+  file: vscode.Uri,
+  content: string,
+  key: string
+) {
+  const document = await vscode.workspace.openTextDocument(file.fsPath);
+  const editor = await vscode.window.showTextDocument(document);
+
+  // Find the line number containing the key
+  let lineNumber = findLineNumber(content, key.split("."));
+
+  if (lineNumber !== -1) {
+    navigateToLine(editor, lineNumber);
+  }
+  return lineNumber;
+}
+
+function navigateToLine(editor: vscode.TextEditor, lineNumber: number) {
+  const position = new vscode.Position(lineNumber, 0);
+  editor.selection = new vscode.Selection(position, position);
+  editor.revealRange(
+    new vscode.Range(position, position),
+    vscode.TextEditorRevealType.InCenter
+  );
 }
 
 function findValue(obj: any, keys: string[]): any {
